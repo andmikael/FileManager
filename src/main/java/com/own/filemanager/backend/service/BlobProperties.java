ab910525eff5387440243d5c7341bc9e8d84a7d1
spsykoosi <andersson.mikael00@gmail.com>
2024-11-01 16:17:31 +0200

    test

--- src/main/java/com/own/filemanager/backend/service/BlobProperties.java
+++ src/main/java/com/own/filemanager/backend/service/BlobProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("blob")
 public class BlobProperties {
     private String connectionStr = null;
-    private String urlPrefix = null;
+    private String endpointString = null;
     
 
     public String getConnectionStr() {
@@ -14,14 +14,16 @@ public class BlobProperties {
 
     public void setConnectionStr() {
         // Grab azure key from env variable
-        this.connectionStr = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
+        this.connectionStr = "DefaultEndpointsProtocol=https;AccountName=thisisnottaken;AccountKey=cS9lLDLb/BF6VS33Fm6Yxzl7kYxdtzc1k6WORFze4NiIeVJdeOkk0YzC0OPZDE02kP/lInPXi39T+AStXdEvGw==;EndpointSuffix=core.windows.net";
+        //this.connectionStr = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
     }
 
     public String getUrlPrefix() {
-        return this.urlPrefix;
+        return this.endpointString;
     }
 
     public void setUrlPrefix() {
-        this.urlPrefix = System.getenv("AZURE_STORAGE_URL_ENDPOINT");
+        this.endpointString = "https://thisisnottaken.blob.core.windows.net/";
+        //this.urlPrefix = System.getenv("AZURE_STORAGE_URL_ENDPOINT");
     }
 }
